148c52f564b40c06db58504d88ec0120540f4759
mamaragan <hellgate4548@yandex.ru>
2018-03-26 20:07:39 +0700

    add requirements.txt

--- orders/admin.py
+++ orders/admin.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 from django.core.urlresolvers import reverse
+from django.http import HttpResponse
 from django.utils.html import format_html
 from .models import Order, OrderItem
+import csv
+import datetime
 
 
 def OrderDetail(obj):
@@ -27,3 +30,26 @@ class OrderAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Order, OrderAdmin)
+
+
+def ExportToCSV(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; \
+            filename=Orders-{}.csv'.format(datetime.datetime.now().strftime("%d/%m/%Y"))
+    writer = csv.writer(response)
+
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+    # Первая строка- оглавления
+    writer.writerow([field.verbose_name for field in fields])
+    # Заполняем информацией
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+    ExportToCSV.short_description = 'Export CSV'
